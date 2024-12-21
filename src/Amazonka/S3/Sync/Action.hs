e0e7dc81fa2be19396d016c71625ce55daf44c1d
patrick brisbin <pbrisbin@gmail.com>
2024-12-21 11:30:35 -0500

    Finish out the udefineds

--- src/Amazonka/S3/Sync/Action.hs
+++ src/Amazonka/S3/Sync/Action.hs
@@ -20,62 +20,90 @@ data Action
   | DeleteObject (BucketKey Abs Object)
   | CopyObjectToFile (BucketKey Abs Object) (Path Abs File)
   | CopyFileToObject (Path Abs File) (BucketKey Abs Object)
+  | CopyObjectToObject (BucketKey Abs Object) (BucketKey Abs Object)
   deriving stock (Eq, Show)
+
+{- FOURMOLU_DISABLE -}
 
 instance ToText Action where
   toText = \case
-    DeleteFile a -> "delete: " <> toText a
-    DeleteObject a -> "delete: " <> toText a
-    CopyObjectToFile a b -> "upload: " <> toText a <> " to " <> toText b
-    CopyFileToObject a b -> "upload: " <> toText a <> " to " <> toText b
+    DeleteFile a           -> "delete:   " <> toText a
+    DeleteObject a         -> "delete:   " <> toText a
+    CopyObjectToFile a b   -> "download: " <> toText a <> " to " <> toText b
+    CopyFileToObject a b   -> "upload:   " <> toText a <> " to " <> toText b
+    CopyObjectToObject a b -> "copy:     " <> toText a <> " to " <> toText b
+
+{- FOURMOLU_ENABLE -}
 
 actionLocalRemote
   :: Path Abs Dir
   -> BucketKey Abs Prefix
   -> These (SyncItem Path File) (SyncItem Key Object)
   -> Action
-actionLocalRemote dir prefix = \case
-  This item ->
-    let
-      file = item.location
-      rel = fileToObject file
-      abs = joinKey prefix.key rel
-    in
-      CopyFileToObject (dir </> file) $ abs `inBucket` prefix.bucket
-  That item ->
-    let
-      rel = item.location
-      abs = joinKey prefix.key rel
-    in
-      DeleteObject $ abs `inBucket` prefix.bucket
-  These sItem tItem ->
-    let
-      file = sItem.location
-      rel = tItem.location
-      abs = joinKey prefix.key rel
-    in
-      CopyFileToObject (dir </> file) $ abs `inBucket` prefix.bucket
+actionLocalRemote =
+  actionLogic
+    (</>)
+    (\bk k -> joinKey bk.key k `inBucket` bk.bucket)
+    fileToObject
+    DeleteObject
+    CopyFileToObject
 
 actionRemoteLocal
   :: BucketKey Abs Prefix
   -> Path Abs Dir
   -> These (SyncItem Key Object) (SyncItem Path File)
   -> Action
-actionRemoteLocal = undefined
+actionRemoteLocal =
+  actionLogic
+    (\bk k -> joinKey bk.key k `inBucket` bk.bucket)
+    (</>)
+    objectToFile
+    DeleteFile
+    CopyObjectToFile
 
 actionRemoteRemote
   :: BucketKey Abs Prefix
   -> BucketKey Abs Prefix
   -> These (SyncItem Key Object) (SyncItem Key Object)
   -> Action
-actionRemoteRemote = undefined
+actionRemoteRemote =
+  actionLogic
+    (\bk k -> joinKey bk.key k `inBucket` bk.bucket)
+    (\bk k -> joinKey bk.key k `inBucket` bk.bucket)
+    id
+    DeleteObject
+    CopyObjectToObject
+
+actionLogic
+  :: (sBucketKey Abs sPrefix -> sKey Rel sObject -> sBucketKey Abs sObject)
+  -> (tBucketKey Abs tPrefix -> tKey Rel tObject -> tBucketKey Abs tObject)
+  -> (sKey Rel sObject -> tKey Rel tObject)
+  -> (tBucketKey Abs tObject -> Action)
+  -> (sBucketKey Abs sObject -> tBucketKey Abs tObject -> Action)
+  -> sBucketKey Abs sPrefix
+  -> tBucketKey Abs tPrefix
+  -> These (SyncItem sKey sObject) (SyncItem tKey tObject)
+  -> Action
+actionLogic inSourceFn inTargetFn transFn deleteFn copyFn source target = \case
+  This item ->
+    copyFn (inSourceFn source item.location)
+      $ inTargetFn target
+      $ transFn item.location
+  That item -> deleteFn $ inTargetFn target item.location
+  These sItem tItem -> copyFn (inSourceFn source sItem.location) $ inTargetFn target tItem.location
 
 fileToObject :: Path Rel File -> Key Rel Object
-fileToObject file =
-  either (error . show) id
-    $ parseRelObject
-    $ ObjectKey
-    $ toCompareKey file
+fileToObject = viaCompareKey $ parseRelObject . ObjectKey
+
+objectToFile :: Key Rel Object -> Path Rel File
+objectToFile = viaCompareKey $ parseRelFile . unpack
+
+viaCompareKey
+  :: (Show e, ToCompareKey (k1 Rel t1))
+  => (Text -> Either e (k2 Rel t2))
+  -> k1 Rel t1
+  -> k2 Rel t2
+viaCompareKey f = either (error . show) id . f . toCompareKey
 
 executeAction :: MonadOutput m => SyncOptions -> Action -> m ()
 executeAction options
