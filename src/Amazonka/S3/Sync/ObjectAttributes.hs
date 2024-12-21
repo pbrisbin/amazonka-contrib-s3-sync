6fc2d12de435bf6b8e8f69989a5946e019db0c1b
patrick brisbin <pbrisbin@gmail.com>
2024-12-21 10:05:01 -0500

    Handle prefixes correctly

--- src/Amazonka/S3/Sync/ObjectAttributes.hs
+++ src/Amazonka/S3/Sync/ObjectAttributes.hs
@@ -11,12 +11,14 @@ import Amazonka.S3.ListObjectsV2
   ( ListObjectsV2Response
   , listObjectsV2Response_commonPrefixes
   , listObjectsV2Response_contents
+  , listObjectsV2Response_prefix
   , listObjectsV2_delimiter
   , listObjectsV2_prefix
   , newListObjectsV2
   )
 import Amazonka.S3.Sync.Key
 import qualified Amazonka.S3.Types as S3
+import Amazonka.S3.Types.CommonPrefix (commonPrefix_prefix)
 import Amazonka.S3.Types.Object (object_key, object_lastModified, object_size)
 import Conduit
 import qualified Control.Monad.AWS as AWS
@@ -42,7 +44,7 @@ listPrefixWithObjectAttributes
 listPrefixWithObjectAttributes bk = do
   runConduit
     $ (AWS.paginateEither req >>= either throwM pure)
-      .| responseToEither
+      .| responseToEither bk
       .| eitherToResult
       .| foldC
  where
@@ -54,30 +56,38 @@ listPrefixWithObjectAttributes bk = do
   mPrefix = toText . fromRelPrefix <$> stripProperPrefix rootKey bk.key
 
 responseToEither
-  :: Monad m
-  => ConduitT ListObjectsV2Response (Either S3.CommonPrefix S3.Object) m ()
-responseToEither = awaitForever $ \r -> do
-  traverse_ (yieldMany . map Left) $ r ^. listObjectsV2Response_commonPrefixes
-  traverse_ (yieldMany . map Right) $ r ^. listObjectsV2Response_contents
-
-eitherToResult
   :: MonadThrow m
-  => ConduitT
-      (Either S3.CommonPrefix S3.Object)
-      ([BucketKey Abs Prefix], [(Key Abs Object, ObjectAttributes)])
+  => BucketKey Abs Prefix
+  -> ConduitT
+      ListObjectsV2Response
+      (Either (BucketKey Abs Prefix) (Key Abs Object, ObjectAttributes))
       m
       ()
+responseToEither bk = awaitForever $ \r -> do
+  for_ (r ^. listObjectsV2Response_commonPrefixes) $ \commonPrefixes -> do
+    for_ commonPrefixes $ \commonPrefix -> do
+      for_ (commonPrefix ^. commonPrefix_prefix) $ \t -> do
+        rel <- parseRelPrefix $ S3.ObjectKey t
+        yield $ Left $ joinKey rootKey rel `inBucket` bk.bucket
+
+  for_ (r ^. listObjectsV2Response_contents) $ \objs -> do
+    for_ objs $ \obj -> do
+      let
+        k = obj ^. object_key
+
+        -- S3 returns the prefix itself as a key, which we should ignore. You
+        -- can confirm this by comparing `aws s3 ls s3://b/p/` with `aws s3api
+        -- list-objects-v2 --bucket b --delimiter / --prefix p`. The latter
+        -- includes this key, but the former does not.
+        isPrefixKey = r ^. listObjectsV2Response_prefix == Just (toText k)
+
+      unless isPrefixKey $ do
+        rel <- parseRelObject $ obj ^. object_key
+        yield $ Right (joinKey rootKey rel, getObjectAttributes obj)
+
+eitherToResult :: Monad m => ConduitT (Either a b) ([a], [b]) m ()
 eitherToResult =
-  awaitForever
-    $ either
-      (yield . (\p -> ([p], [])) <=< commonPrefixToPrefix)
-      (yield . (\i -> ([], [i])) <=< objectToItem)
-
-commonPrefixToPrefix :: S3.CommonPrefix -> m (BucketKey Abs Prefix)
-commonPrefixToPrefix = undefined
-
-objectToItem
-  :: MonadThrow m => S3.Object -> m (Key Abs Object, ObjectAttributes)
-objectToItem obj = do
-  rel <- parseRelObject $ obj ^. object_key
-  pure (joinKey rootKey rel, getObjectAttributes obj)
+  awaitForever $ either (yield . toFstSingleton) (yield . toSndSingleton)
+ where
+  toFstSingleton x = ([x], [])
+  toSndSingleton y = ([], [y])
